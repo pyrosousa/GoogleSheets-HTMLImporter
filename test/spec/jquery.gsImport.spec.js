eb80f25bb5c416aabbbe6be0ae5234d3bdd2e79b
seanmcardwell <sean.m.cardwell@gmail.com>
2017-05-11 11:36:35 -0400

    starting to expand picturize options

--- test/spec/jquery.gsImport.spec.js
+++ test/spec/jquery.gsImport.spec.js
@@ -1,183 +1,205 @@
-;(function($) {
-	"use strict";
-
-	$.fn.gsImport = function( options ) {
-
-		var defaults = {
-			class: {
-	            "cell": "gsCell",
-	            "row": "gsRow",
-	            "header": "gsHeader",
-	            "headerRow": "gsHeaderRow",
-	            "body": "gsBody",
-	            "table": "gsTable table table-responsive table-striped",
-			},
-			searchBar: 0,
-			picturize: true,
-            image1: "<i class=\"fa fa-check check-icon\"></i>",
-            image2: "<i class=\"fa fa-times x-icon\"></i>",
+// ;
+(function ($) {
+    "use strict";
+    $.fn.gsImport = function (options) {
+        var defaults = {
+            class: {
+                cell: "gsCell",
+                row: "gsRow",
+                header: "gsHeader",
+                headerRow: "gsHeaderRow",
+                body: "gsBody",
+                table: "gsTable table table-responsive table-striped"
+            },
+            general: {
+                searchBar: 0,
+                hyperlink: "hyperlink",
+            },
+            picturize: {
+                toggle: "true",
+                image1: ["!", "<i class=\"fa fa-check check-icon\"></i>"],
+                image2: ["X", "<i class=\"fa fa-times x-icon\"></i>"],
+            },
             customImages: "",
             ignore: {
-            	"toggle": 0,
-            	"column": "",
-            	"trigger": "",
+                column: "",
+                trigger: ""
             }
         };
 
-		var settings = $.extend(true, {}, defaults, options );
+        var settings = $.extend(true, {}, defaults, options);
 
-		var tagClassRE = /^[A-Za-z][ -_A-Za-z0-9]+$/;
+        var tagClassRE = new RegExp("^[A-Za-z][ -_A-Za-z0-9]+$");
 
-		$.each(settings, function validateOptions(key, val) {
+        $.each(settings, function validateOptions(key, val) {
+            if (key === "class") {
+                if (typeof val.table !== "string" || val.table.match(tagClassRE) === null) {
+                    settings.class.table = defaults.class.table;
+                    console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for Class.table.");
+                }
+                if (typeof val.cell !== "string" || val.cell.match(tagClassRE) === null) {
+                    settings.class.cell = defaults.class.cell;
+                    console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for Class.cell.");
+                }
+                if (typeof val.row !== "string" || val.row.match(tagClassRE) === null) {
+                    settings.class.row = defaults.class.row;
+                    console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for Class.row.");
+                }
+                if (typeof val.header !== "string" || val.header.match(tagClassRE) === null) {
+                    settings.class.header = defaults.class.header;
+                    console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for Class.header.");
+                }
+                if (typeof val.headerRow !== "string" || val.headerRow.match(tagClassRE) === null) {
+                    settings.class.headerRow = defaults.class.headerRow;
+                    console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for Class.headerRow.");
+                }
+                if (typeof val.body !== "string" || val.body.match(tagClassRE) === null) {
+                    settings.class.body = defaults.key.body;
+                    console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for Class.body.");
+                }
+            }
+            if (key === "searchBar") {
+                if (val !== 1 && val !== 0) {
+                    settings.general.searchBar = defaults.general.searchBar;
+                    console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for " + key + ".");
+                }
+            }
+            // if (key === "picturize") {
+            //     if (typeof(key.toggle) !== "boolean") {
+            //         settings.picturize = defaults.picturize;
+            //         console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for " + key + ".");
+            //     }
+            // }
+        });
 
-			if (key === "class") {
 
-				if (typeof val.table !== "string" || val.table.match(tagClassRE) === null){
-					settings.class.table = defaults.class.table;
-					console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for Class.table.");
-				}
-				if (typeof val.cell !== "string" || val.cell.match(tagClassRE) === null){
-					settings.class.cell = defaults.class.cell;
-					console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for Class.cell.");
-				}
-				if (typeof val.row !== "string" || val.row.match(tagClassRE) === null){
-					settings.class.row = defaults.class.row;
-					console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for Class.row.");
-				}
-				if (typeof val.header !== "string" || val.header.match(tagClassRE) === null){
-					settings.class.header = defaults.class.header;
-					console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for Class.header.");
-				}
-				if (typeof val.headerRow !== "string" || val.headerRow.match(tagClassRE) === null){
-					settings.class.headerRow = defaults.class.headerRow;
-					console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for Class.headerRow.");
-				}
-				if (typeof val.body !== "string" || val.body.match(tagClassRE) === null){
-					settings.class.body = defaults.key.body;
-					console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for Class.body.");
-				}
-			}
-			if (key === "searchBar"){
-				if (val !== 1 && val !== 0){
-					settings.searchBar = defaults.searchBar;
-					console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for " + key + ".");
-				}	
-			}
-			if (key === "picturize"){
-				if (typeof(val) !== "boolean") {
-					settings.picturize = defaults.picturize;
-					console.warn("GSImport:\t One of your GSImport options might be invalid.\n\t\t\t Resetting to default value for " + key + ".");
-				}	
-			}
-			
-		});
+        var map = new Map(Object.entries(settings.picturize));
+        // map.delete('toggle');
+        console.log(map.entries());
 
         return this.append(function gsImporter() {
-			var $me = $(this),
-				headers = $me.data("headers"),
-				key = $me.data("key"),
-				index = $me.data("index");
+            var $me = $(this),
+                headers = $me.data("headers"),
+                key = $me.data("key"),
+                index = $me.data("index");
 
-			if (typeof key === "undefined") {
-			    console.error("GSImport: No data-key provided for div: " + $me.attr("id"));
-			    return;
-			}
+            if (key === undefined || key === null) {
+                console.error("GSImport: No data-key provided for div: " + $me.attr("id"));
+                return;
+            }
 
-			if (typeof headers === "undefined") {
-			    console.error("GSImport: No data-headers provided for div: " + $me.attr("id"));
-			    return;
-			}
+            if (headers === undefined || key === null) {
+                console.error("GSImport: No data-headers provided for div: " + $me.attr("id"));
+                return;
+            }
 
-			if (typeof index === "undefined") {
-			    console.warn("GSImport: No data-index provided for div: " + $me.attr("id") + "\nDefaulting to od6");
-			    index = "od6";
-			}
+            if (index === undefined || key === null) {
+                console.warn("GSImport: No data-index provided for div: " + $me.attr("id") + "\nDefaulting to od6");
+                index = "od6";
+            }
 
-			var url = "https://spreadsheets.google.com/feeds/list/" + key + "/" + index + "/public/values?alt=json";
+            var url = "https://spreadsheets.google.com/feeds/list/" + key + "/" + index + "/public/values?alt=json";
 
-			settings.ignore.trigger.toLowerCase();
-			
-			// console.log(url);
+            settings.ignore.trigger.toLowerCase();
 
-			var headerList = headers.split(", ");
+            var headerList = headers.replace(/\s/g, "").split(",");
 
-			function picturize(nStr) {
+            function picturizeItem(nStr) {
 
-				if ( Boolean(settings.picturize) === false ){
-					if (picturizeLog < 1){
-						console.log("GSImport: Picturize disabled for div: " + $me.attr("id"));
-						picturizeLog += 1;
-					}
-					return nStr;
-				}
+                if (Boolean(settings.picturize.toggle) === false) {
+                    var picturizeLog = 0;
+                    if (picturizeLog < 1) {
+                        console.log("GSImport: Picturize disabled for div: " + $me.attr("id"));
+                        picturizeLog += 1;
+                    }
+                    return nStr;
+                }
 
-				switch (nStr)
-				{
-					case "":
-						return "-";
-					case "!":
-						return settings.image1;
-					case "X":
-						return settings.image2;
-					default:
-						return nStr;
-				}
-			}
+                /*
+                For trigger in picturize
+                    If this cell equals trigger#
+                    return image#
+                    else if cell equals ""
+                        return  -
+                    else return cell
+                */
 
-			var sheetHTML = "";
 
-			$.getJSON(url, function(data) {
-				var entry = data.feed.entry;
+                switch (nStr) {
+                case "":
+                    return "-";
+                case settings.picturize.trigger1:
+                    return settings.picturize.image1;
+                case settings.picturize.trigger2:
+                    return settings.picturize.image2;
+                default:
+                    return nStr;
+                }
+            }
 
-				sheetHTML += "<table class=\"" + settings.class.table + "\"><thead><tr class=\"" + settings.class.headerRow + "\">";
+            var sheetHTML = "";
 
-				$.each(headerList, function() {
-					sheetHTML += "<th class=\"" + settings.class.header + "\">" + this.toString()+"</th>";
-				});
+            $.getJSON(url, function (data) {
+                var entry = data.feed.entry;
 
-				var headerCounter = 0;
+                sheetHTML += "<table class=\"" + settings.class.table + "\"><thead><tr class=\"" + settings.class.headerRow + "\">";
 
-				$.each(headerList, function () {
-					headerList[headerCounter] = this.replace(/^\d+ */g, "").replace(/\s/g, "");
-					headerCounter ++;
-				});
+                $.each(headerList, function () {
+                    sheetHTML += "<th class=\"" + settings.class.header + "\">" + this.toString() + "</th>";
+                });
 
-				sheetHTML += "</tr></thead><tbody class=\""+ settings.class.body + "\">";
+                var headerCounter = 0;
 
-				if ( Boolean(settings.ignore.toggle) === true ){
-					$(entry).each(function(){
-						var $curRow = this;
-						if ( $curRow["gsx$"+settings.ignore.column.toLowerCase()].$t.toLowerCase() !== settings.ignore.trigger.toLowerCase() ) {
-							sheetHTML += "<tr>";
-							$(headerList).each(function () {
-								var thisHeader = this;
-								sheetHTML += "<td class=\"" + settings.class.cell + "\">" + picturize($curRow["gsx$"+thisHeader.toString().toLowerCase()].$t) + "</td>";
-							});
-							sheetHTML+="</tr>";
-						}
-					});
-				} else {
-					$(entry).each(function(){
-						var $curRow = this;
-						sheetHTML += "<tr>";
-						$(headerList).each(function () {
-							var thisHeader = this;
-							sheetHTML += "<td class=\"" + settings.class.cell + "\">" + picturize($curRow["gsx$"+thisHeader.toString().toLowerCase()].$t) + "</td>";
-						});
-						sheetHTML+="</tr>";
-					});
-				}
+                $.each(headerList, function () {
+                    // var replaceStartingDigits = newReg
+                    headerList[headerCounter] = this.replace(/^\d+ */g, "").replace(/\s/g, "");
+                    headerList[headerCounter] = headerList[headerCounter].toLowerCase();
+                    headerCounter += 1;
+                });
 
-				
+                sheetHTML += "</tr></thead><tbody class=\"" + settings.class.body + "\">";
 
-				sheetHTML += "</tbody></table>";
+                if (settings.ignore.column !== null && settings.ignore.column !== "" && settings.ignore.trigger !== null && settings.ignore.trigger !== "") {
+                    $(entry).each(function () {
+                        var $curRow = this;
+                        if ($curRow["gsx$" + settings.ignore.column.toLowerCase()].$t.toLowerCase() !== settings.ignore.trigger.toLowerCase()) {
+                            sheetHTML += "<tr>";
+                            var firstColumnDone = false;
+                            $(headerList).each(function () {
+                                var thisHeader = this;
+                                if (firstColumnDone !== true) {
+                                    if ($curRow["gsx$" + settings.general.hyperlink.toLowerCase()].$t !== null) {
+                                        sheetHTML += "<td class=\"" + settings.class.cell + "\"><a href='" + $curRow["gsx$" + settings.general.hyperlink.toLowerCase()].$t +"'>" + picturizeItem($curRow["gsx$" + thisHeader.toString().toLowerCase()].$t) + "</a></td>";
+                                    } else {
+                                        sheetHTML += "<td class=\"" + settings.class.cell + "\">" + picturizeItem($curRow["gsx$" + thisHeader.toString().toLowerCase()].$t) + "</td>";    
+                                    }
+                                } else {
+                                    sheetHTML += "<td class=\"" + settings.class.cell + "\">" + picturizeItem($curRow["gsx$" + thisHeader.toString().toLowerCase()].$t) + "</td>";    
+                                }
+                                firstColumnDone = true;
+                            });
+                            sheetHTML += "</tr>";
+                        }
+                    });
+                } else {
+                    $(entry).each(function () {
+                        var $curRow = this;
+                        sheetHTML += "<tr>";
+                        $(headerList).each(function () {
+                            var thisHeader = this;
+                            sheetHTML += "<td class=\"" + settings.class.cell + "\">" + picturizeItem($curRow["gsx$" + thisHeader.toString().toLowerCase()].$t) + "</td>";
+                        });
+                        sheetHTML += "</tr>";
+                    });
+                }
 
-				$($me).append(sheetHTML);
+                sheetHTML += "</tbody></table>";
 
-				console.log("GS Import: " + $me.attr("id") +" key(" + $me.data("key") + ") loaded");
-			});				
+                $($me).append(sheetHTML);
 
-		return sheetHTML;
-		});
-	};
+                console.log("GS Import: " + $me.attr("id") + " key(" + $me.data("key") + ") loaded");
+            });
+            return sheetHTML;
+        });
+    };
 }(jQuery));
